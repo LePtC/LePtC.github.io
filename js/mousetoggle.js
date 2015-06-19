a5c9eb372b009b40a059a6878af84742adbf2d0a
萌狸 <alileptc@gmail.com>
2015-06-20 00:56:49 +0800

    wiki homepage upgrade

---
+++ js/mousetoggle.js
@@ -0,0 +1,9 @@
+// mousetoggle.js for LePtC [2015.0317]
+// 代码来自 http://stackoverflow.com/questions/2707100/how-to-show-hidden-divs-on-mouseover
+
+function show(id) {
+  document.getElementById(id).style.visibility = "visible";
+}
+function hide(id) {
+  document.getElementById(id).style.visibility = "hidden";
+}
