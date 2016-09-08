30f9333107092914277f1e6f8414c9394cc437ca
萌狸 <alileptc@gmail.com>
2016-09-08 22:00:50 +0800

    将置顶也做进数据库

--- js/mousetoggle.js
+++ js/mousetoggle.js
@@ -1,4 +1,4 @@
-// mousetoggle.js for LePtC [2015.0317]
+// mousetoggle.js for LePtC [2015.03.17]
 // 代码来自 http://stackoverflow.com/questions/2707100/how-to-show-hidden-divs-on-mouseover
 
 function show(id) {
@@ -6,4 +6,16 @@ function show(id) {
 }
 function hide(id) {
   document.getElementById(id).style.visibility = "hidden";
+}
+
+// [2016.09.08]
+// http://www.dustindiaz.com/seven-togglers/
+
+function toggle(obj) {
+  var el = document.getElementById(obj);
+  if ( el.style.display != 'block' ) {
+    el.style.display = 'block';
+  } else {
+    el.style.display = 'none';
+  }
 }
